d3226c8fd6d908931ca957d81080ac74a962f76d
ducnc <ducmam97@gmail.com>
2021-06-20 16:37:09 +0700

    a

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // if (Auth::user()->role != User::ADMIN) {
+        //     return redirect()->route('english.vocabulary.index');
+        // }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\GeneralController;
+use App\Http\Controllers\VocabularyController;
+use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +24,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::middleware('throttle:vocabulary')->group(function () {
+    Route::prefix('english')->group(function () {
+        Route::name('english.')->group(function () {
+            Route::resource('vocabulary', VocabularyController::class)->only(['index', 'store', 'destroy']);
+            Route::post('vocabulary/forward', [VocabularyController::class, 'forward'])->name('vocabulary.forward');
+            Route::get('vocabulary/mergesound/{day}', [VocabularyController::class, 'mergeSound'])
+                ->name('vocabulary.mergesound')->whereNumber('day');
+        });
+    });
+});
+
+Route::resource('general', GeneralController::class)->except(['create', 'show', 'edit', 'update']);
+
+Route::fallback(function () {
+    return redirect()->route('english.vocabulary.index');
+});
