7e037f1707e11c3c6cbf514827ad510eda1afec8
ducnc <ducmam97@gmail.com>
2021-07-12 19:44:13 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <html>
 
-<body style="margin-left: 0px; margin-top: 0; margin-right: 0; margin-bottom: 0; border: 2px white solid; background-color: black;">
-    <div>sgsdfgfdg</div>
+<body style="margin-left: 0px; margin-top: 0; margin-right: 0; margin-bottom: 0">
+<div style="word-wrap: break-word;">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
 </body>
 
 </html>
