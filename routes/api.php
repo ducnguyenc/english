6c3a1da94363c0df29000fc03f1b0c8e90ff32af
ducnc <ducmam97@gmail.com>
2021-11-10 22:43:57 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->group(function () {
+// Route::middleware(['auth:sanctum'])->group(function () {
     Route::prefix('english')->group(function () {
         Route::name('english.')->group(function () {
             Route::resource('vocabulary', VocabularyController::class)->only(['index', 'store', 'destroy']);
@@ -30,7 +30,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
             Route::resource('sinus', SinusController::class)->only(['index', 'store', 'destroy']);
         });
     });
-});
+// });
 
 Route::fallback(function () {
     return redirect()->route('english.vocabulary.index');
